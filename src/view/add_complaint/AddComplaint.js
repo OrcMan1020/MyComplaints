298b7e0e57317ed67bb0991724b155bee04e1c75
saix <sai.xu@citrix.com>
2018-03-14 23:27:24 +0800

    111

--- src/view/add_complaint/AddComplaint.js
+++ src/view/add_complaint/AddComplaint.js
@@ -4,6 +4,7 @@
 import React, { Component } from 'react';
 import { List, Card, WhiteSpace, WingBlank, InputItem, TextareaItem, Picker, ImagePicker,
      Checkbox, Flex, Button } from 'antd-mobile';
+import SuccessModel from './SuccessModel';
 
 import './AddComplaint.css';
 
@@ -20,7 +21,8 @@ class AddComplaint extends Component {
         super(prop);
         this.state = {
             files: data,
-            agreed: true
+            agreed: true,
+            visible: false
         };
     }
 
@@ -37,97 +39,104 @@ class AddComplaint extends Component {
 
         return (
 
-            <div class='complaint'>
-                <WhiteSpace size="lg"/>
+            <div>
+                <SuccessModel visible={this.state.visible}/>
+                <div class='complaint'>
+                    <WhiteSpace size="lg"/>
 
-                <List className="my-list">
-                    <InputItem
-                        placeholder= '(必填) 填写投诉对象'
-                    >投诉对象</InputItem>
+                    <List className="my-list">
+                        <InputItem
+                            placeholder= '(必填) 填写投诉对象'
+                        >投诉对象</InputItem>
 
-                    <Picker title="投诉问题">
-                        <List.Item arrow="horizontal">投诉问题<span style={{paddingLeft:'30px', color:'#bbb'}}>(必填) 选择投诉问题的核心</span></List.Item>
-                    </Picker>
+                        <Picker title="投诉问题">
+                            <List.Item arrow="horizontal">投诉问题<span style={{paddingLeft:'30px', color:'#bbb'}}>(必填) 选择投诉问题的核心</span></List.Item>
+                        </Picker>
 
-                    <InputItem
-                        placeholder= '(必填) 填写投诉要求, 35字以内'
-                    >投诉诉求</InputItem>
-                    <TextareaItem
-                        title="投诉详情"
-                        placeholder="(必填) 请完整描述投诉事件"
-                        rows="5"
-                    />
-                    <InputItem
-                        placeholder= '(必填) 填写个人手机号'
-                    >联系电话</InputItem>
-                </List>
-
-                <WhiteSpace size="lg"/>
-
-                <List className="my-list">
-
-                    <InputItem
-                        placeholder= '例如银行账号, 快递单号'
-                        type = 'password'
-                    >隐藏内容</InputItem>
-
-
-                    <Item>
-                        <div>
-                            添加附件
-                            <span style={{paddingLeft:'30px', color:'#bbb'}}>支持jpg,png,gif格式, 最多上传10张</span>
-
-                        </div>
-
-                        <ImagePicker
-                            files={files}
-                            onChange={this.onChange}
-                            onImageClick={(index, fs) => console.log(index, fs)}
-                            selectable={files.length <= 10}
+                        <InputItem
+                            placeholder= '(必填) 填写投诉要求, 35字以内'
+                        >投诉诉求</InputItem>
+                        <TextareaItem
+                            title="投诉详情"
+                            placeholder="(必填) 请完整描述投诉事件"
+                            rows="5"
                         />
+                        <InputItem
+                            placeholder= '(必填) 填写个人手机号'
+                        >联系电话</InputItem>
+                    </List>
+
+                    <WhiteSpace size="lg"/>
+
+                    <List className="my-list">
+
+                        <InputItem
+                            placeholder= '例如银行账号, 快递单号'
+                            type = 'password'
+                        >隐藏内容</InputItem>
 
 
-                    </Item>
-                    <Item>
-                        <div>
-                            隐私附件
-                            <span style={{paddingLeft:'30px', color:'#bbb'}}>不公开给第三方, 最多上传10张</span>
+                        <Item>
+                            <div>
+                                添加附件
+                                <span style={{paddingLeft:'30px', color:'#bbb'}}>支持jpg,png,gif格式, 最多上传10张</span>
 
-                        </div>
+                            </div>
 
-                        <ImagePicker
-                            files={files}
-                            onChange={this.onChange}
-                            onImageClick={(index, fs) => console.log(index, fs)}
-                            selectable={files.length <= 10}
-                        />
+                            <ImagePicker
+                                files={files}
+                                onChange={this.onChange}
+                                onImageClick={(index, fs) => console.log(index, fs)}
+                                selectable={files.length <= 10}
+                            />
 
 
-                    </Item>
+                        </Item>
+                        <Item>
+                            <div>
+                                隐私附件
+                                <span style={{paddingLeft:'30px', color:'#bbb'}}>不公开给第三方, 最多上传10张</span>
 
-                </List>
+                            </div>
 
-                <WhiteSpace size="md"/>
+                            <ImagePicker
+                                files={files}
+                                onChange={this.onChange}
+                                onImageClick={(index, fs) => console.log(index, fs)}
+                                selectable={files.length <= 10}
+                            />
 
-                <Flex>
-                    <Flex.Item>
-                        <AgreeItem data-seed="logId"
-                                   checked={this.state.enableSubmit}
-                                   onChange={e => {this.state.enableSubmit=!this.state.enableSubmit; this.setState({
-                            enableSubmit :  this.state.enableSubmit}
 
-                        )}}>
-                            同意微投诉使用协议
-                        </AgreeItem>
-                    </Flex.Item>
-                </Flex>
+                        </Item>
 
-                <div>
-                    <WingBlank size="lg"/>
+                    </List>
+
+                    <WhiteSpace size="md"/>
+
+                    <Flex>
+                        <Flex.Item>
+                            <AgreeItem data-seed="logId"
+                                       checked={this.state.enableSubmit}
+                                       onChange={e => {this.state.enableSubmit=!this.state.enableSubmit; this.setState(
+
+                                       )}}>
+                                同意微投诉使用协议
+                            </AgreeItem>
+                        </Flex.Item>
+                    </Flex>
+
+                    <div>
+                        <WingBlank size="lg"/>
                         <Button className="btn" type="primary"
-                                disabled={!this.state.enableSubmit}>提交</Button>
+                                disabled={!this.state.enableSubmit}
+                                onClick={e=>{
+                                    this.state.visible = true;
+                                    this.setState()}}
+                                >提交</Button>
+                    </div>
                 </div>
             </div>
+
 
         )
 
---
+++ src/view/add_complaint/SuccessModel.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+import { Modal, WhiteSpace, Grid  } from 'antd-mobile';
+
+
+class SuccessModel extends Component
+{
+    constructor(prop){
+        super(prop)
+        this.state = {
+            visible : this.props.visible
+        }
+
+
+    }
+
+    render() {
+
+        const icons = [
+            {icon : './img/wechat.png'},
+            {icon : './img/weibo.png'},
+            {icon : './img/unknown.png'},
+            {icon : './img/qq.png'},
+
+        ];
+
+        return (
+                    <div>
+                        <Modal
+                            wrapClassName ='model-wrap'
+                            visible={this.props.visible}
+                            transparent
+                        >
+                            <div>
+                                <img src='./img/model-header.png'/>
+                                <WhiteSpace size='lg'/>
+                                <div style={{fontSize:'17px', color:'#000'}}><b>已提交成功, 请等待受理...</b></div>
+                                <WhiteSpace/>
+                                <div>可在"我的"中查看受理情况</div>
+                                <WhiteSpace size='lg'/>
+                                <WhiteSpace size='lg'/>
+                                <div>-------------&nbsp;&nbsp;选择分享到:&nbsp;&nbsp;--------------</div>
+                                <WhiteSpace size='lg'/>
+
+                                <Grid data={icons}
+                                      hasLine={false}
+                                      activeStyle={false}
+                                      renderItem={dataItem => (
+                                          <div >
+                                              <img src={dataItem.icon} style={{ width: '52px', height: '52px' }} alt=""
+                                              onClick={(e)=>console.log(dataItem.icon)}/>
+                                          </div>
+                                      )}
+                                />
+                            </div>
+                        </Modal>
+                    </div>
+
+        );
+    }
+
+}
+
+export default SuccessModel;
